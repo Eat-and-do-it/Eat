2c73b6984931ed5a8cf3b2a050ee2d3624eb36e1
Minhye <amydreamsu@gmail.com>
2024-04-26 22:56:54 +0900

    배포시 사진경로

--- eating/src/main/java/com/project/eat/review/WebConfig.java
+++ eating/src/main/java/com/project/eat/review/WebConfig.java
@@ -1,18 +1,25 @@
 package com.project.eat.review;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Slf4j
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     private String connectPath = "/uploadimgPath/**";
-    private String resourcePath = "file:///C:/upload/";
+ //   private String resourcePath = "file:///C:/upload/";
+//    private String resourcePath = "file:///root/upload/";
+    @Value("${upload.dir}")
+    private String uploadDir;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.info("{}",uploadDir);
         registry.addResourceHandler(connectPath)
-                .addResourceLocations(resourcePath);
+                .addResourceLocations("file:///"+uploadDir);
     }
 }
